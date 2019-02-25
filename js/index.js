1be00adf3ec19ad123ed96cdbca79d59d4b4fdf2
Soontrax <32308953+Soontrax@users.noreply.github.com>
2019-02-25 15:40:22 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
     $("#carouselExampleIndicators").fadeIn(2000);
 })
+
+/*
+$(window).scroll(function() {
+    if($(window).scrollTop() > 0) {
+        headerOff();
+    } else {
+        headerOn();
+    }
+});
+*/
+
