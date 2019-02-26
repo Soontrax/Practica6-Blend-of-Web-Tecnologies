309c3d9ea4b25f1abed13b7d980675f5da214db4
Soontrax <32308953+Soontrax@users.noreply.github.com>
2019-02-26 01:32:39 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
     $("#carouselExampleIndicators").fadeIn(2000);
+    $(".grid-item").fadeIn(1500);
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('.scrolltop:hidden').stop(true, true).fadeIn();
+        } else {
+            $('.scrolltop').stop(true, true).fadeOut();
+        }
+    });
+    $(function () { $(".scroll").click(function () { $("html,body").animate({ scrollTop: $(".thetop").offset().top }, "1000"); return false }) })
 })
 
 /*
