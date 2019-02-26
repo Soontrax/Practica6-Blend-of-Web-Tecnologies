ff6c970556e2e4339c3fdaba5d268b08b52b12a6
Soontrax <32308953+Soontrax@users.noreply.github.com>
2019-02-26 15:31:21 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,6 +2,25 @@ $(document).ready(function () {
     $("#carouselExampleIndicators").fadeIn(2000);
     $(".grid-item").fadeIn(1500);
 
+    //Brands Cards Description
+    $("#abarth").click(function () {
+        $("#card-abarth").toggle("slow");
+    });
+
+    $("#alfa").click(function () {
+        $("#card-alfa").toggle("slow");
+    });
+
+    $("#bmw").click(function () {
+        $("#card-bmw").toggle("slow");
+    });
+
+    $("#am").click(function () {
+        $("#card-am").toggle("slow");
+    });
+
+    //********************************/
+
     $(window).scroll(function () {
         if ($(this).scrollTop() > 500) {
             $('.scrolltop:hidden').stop(true, true).fadeIn();
@@ -9,7 +28,14 @@ $(document).ready(function () {
             $('.scrolltop').stop(true, true).fadeOut();
         }
     });
-    $(function () { $(".scroll").click(function () { $("html,body").animate({ scrollTop: $(".thetop").offset().top }, "1000"); return false }) })
+    $(function () {
+        $(".scroll").click(function () {
+            $("html,body").animate({
+                scrollTop: $(".thetop").offset().top
+            }, "1000");
+            return false
+        })
+    })
 })
 
 /*
@@ -21,4 +47,3 @@ $(window).scroll(function() {
     }
 });
 */
-
